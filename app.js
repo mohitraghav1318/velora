882a8f29c9590ba4ecbe31287b95b8c2bb9bf92c
mohitraghav1318 <mohitraghav1318@gmail.com>
2026-02-11 21:50:06 +0530

    Changing OS

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const ejsMate = require('ejs-mate');
 const ExpressError = require('./utils/ExpressError.js');
 const Session = require('express-session');
 const flash = require('connect-flash');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
 const listings = require('./routes/listings');
 const reviews = require('./routes/review');
@@ -41,6 +44,14 @@ app.get('/', (req, res) => {
 
 app.use(Session(sessionOptions));
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+// for more info go to npm package passport-local-mongoose
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
     res.locals.success = req.flash('success');
